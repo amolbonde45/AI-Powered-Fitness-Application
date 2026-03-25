fcd606368e83337cd486e5c1dc564614aefdbc15
amolkoderz <amolbonde@koderztechnology.com>
2026-03-25 15:45:44 +0530

    Initiated the Spring project For AI-Fitness-App UserService

---
+++ UserService/src/test/java/com/AI_Powered_Fitness_App/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI_Powered_Fitness_App.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
