070f600ff0b1eefbd9ddbcc5c2a97db83837fa34
amolkoderz <amolbonde@koderztechnology.com>
2026-03-30 15:50:47 +0530

    Add new MicroService i.e ActivityService

---
+++ ActivityService/src/main/java/com/AI_Powered_Fitness_App/ActivityService/ActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.AI_Powered_Fitness_App.ActivityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityServiceApplication.class, args);
+	}
+
+}
---
+++ ActivityService/src/test/java/com/AI_Powered_Fitness_App/ActivityService/ActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI_Powered_Fitness_App.ActivityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
