c98dd47cd51dbac8914259380208916147df51f5
amolkoderz <amolbonde@koderztechnology.com>
2026-04-02 13:43:37 +0530

    Added a very important microsservic that is euraka server which will help in getting registered and communicating between services

---
+++ EurekaServer/src/test/java/com/AI_Powered_Fitness_App/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI_Powered_Fitness_App.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
